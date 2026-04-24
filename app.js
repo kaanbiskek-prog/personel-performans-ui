046cabed85824920adb4822051f2200ae8320afc
kaanbiskek-prog <kaanbiskek@gmail.com>
2026-04-24 08:26:53 +0600

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = "BURAYA_APPS_SCRIPT_WEB_APP_URL";
+const API_BASE_URL = "https://script.google.com/macros/s/AKfycbyBmT4GrkMfU0sF_oIPkXMEWvLBgv3C__kg18R2Ji-Hgq3xdx_8Z-435ZTV4H9dd8rJ/exec";
 
 const STATE = {
   tgUser: null,
